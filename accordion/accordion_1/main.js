75ee29b94fb0ee5378c3ab90ef5ef190bb827cf4
daegons <119998807+daegons@users.noreply.github.com>
2023-01-16 18:19:00 +0900

    Add files via upload

---
+++ accordion/accordion_1/main.js
@@ -0,0 +1,9 @@
+const faqContainer = document.querySelectorAll('.faq_container');
+//querySelectorAll = faq_container가 적용된 박스를 전분 가져옴(5개)
+console.log(faqContainer);//faqContainer 노드 리스트 객체로 가져옴
+
+//반복문 순회하면서 해당 FAQ 제목 클릭시 콜백처리
+for(let i = 0; i < faqContainer.length; i++) {
+
+    faqContainer[i]
+}
